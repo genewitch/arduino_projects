ceaf0c2b90f4393a960d9675de212cd65652ba20
genewitch <genewitch@gmail.com>
2019-06-05 19:42:29 -0500

    Create bmp085_gyro.ino

---
+++ bmp085_gyro.ino
@@ -0,0 +1,141 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+/*************************************************** 
+  This is an example for the BMP085 Barometric Pressure & Temp Sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> https://www.adafruit.com/products/391
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
+// Connect GND to Ground
+// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+// EOC is not used, it signifies an end of conversion
+// XCLR is a reset pin, also not used here
+
+//GYRO
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+int Addr = 105;                 // I2C address of gyro
+int x, y, z;
+
+//BORSCHT
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  while (1) {}
+  }
+  writeI2C(CTRL_REG1, 0x1F);    // Turn on all axes, disable power down
+  writeI2C(CTRL_REG3, 0x08);    // Enable control ready signal
+  writeI2C(CTRL_REG4, 0x00);    // Set scale (0x00=250 deg/sec0x80=500)
+  delay(100);                   // Wait to synchronize 
+}
+  
+void loop() {
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    //Serial.println(" *C");
+    Serial.print("\t");
+    
+    Serial.print("Pressure = ");
+    //Serial.print(bmp.readPressure()); //pascal
+    //Serial.print(bmp.readPressure()*0.00029529983071445);   //inHg
+    Serial.print(bmp.readPressure()*0.01);   //mBar
+    
+    //Serial.println(" Pa");
+    Serial.print("\t");
+/*    
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+    Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
+    
+    Serial.println();
+*/
+    delay(500);
+    
+    getGyroValues();              // Get new values
+    // In following Dividing by 114 reduces noise
+    //Serial.print("/114 X:");  
+    Serial.print(x / 114);  
+    Serial.print("\t");
+    
+    //Serial.print(" /114 Y:"); 
+    Serial.print(y / 114);
+    Serial.print("\t");
+    
+    //Serial.print(" /114 Z:"); 
+    Serial.println(z / 114);
+    //Serial.println("\t");
+    
+    //Serial.print("Raw X:");  
+    Serial.print(x);  
+    //Serial.print(" Raw Y:"); 
+    Serial.print("\t");
+    
+    Serial.print(y);
+    Serial.print("\t");
+    //Serial.print(" Raw Z:"); 
+    Serial.println(z);
+}
+
+void getGyroValues () {
+  byte MSB, LSB;
+
+  MSB = readI2C(0x29);
+  LSB = readI2C(0x28);
+  x = ((MSB << 8) | LSB);
+
+  MSB = readI2C(0x2B);
+  LSB = readI2C(0x2A);
+  y = ((MSB << 8) | LSB);
+
+  MSB = readI2C(0x2D);
+  LSB = readI2C(0x2C);
+  z = ((MSB << 8) | LSB);
+}
+
+int readI2C (byte regAddr) {
+    Wire.beginTransmission(Addr);
+    Wire.write(regAddr);                // Register address to read
+    Wire.endTransmission();             // Terminate request
+    Wire.requestFrom(Addr, 1);          // Read a byte
+    while(!Wire.available()) { };       // Wait for receipt
+    return(Wire.read());                // Get result
+}
+
+void writeI2C (byte regAddr, byte val) {
+    Wire.beginTransmission(Addr);
+    Wire.write(regAddr);
+    Wire.write(val);
+    Wire.endTransmission();
+}
